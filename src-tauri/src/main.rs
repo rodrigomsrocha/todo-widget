a1569f68cba0bc29581077216c3a59cedc53ba58
rodrigomsrocha <rodrigomtc82@gmail.com>
2024-02-19 00:41:35 -0300

    feature: app design ready

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,52 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, PhysicalPosition};
+use tauri_plugin_positioner::{WindowExt, Position};
 
 fn main() {
+    let quit = CustomMenuItem::new("quit", "Quit");
+    let system_tray_menu = SystemTrayMenu::new().add_item(quit);
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_positioner::init())
+        .system_tray(SystemTray::new().with_menu(system_tray_menu))
+        .on_system_tray_event(|app, event| {
+            tauri_plugin_positioner::on_tray_event(app, &event);
+            match event {
+                SystemTrayEvent::LeftClick {
+                    position: _,
+                    size: _,
+                    .. 
+                } => {
+                    let window = app.get_window("main").unwrap();
+                    let _ = window.move_window(Position::TrayCenter);
+                    let current_position = window.outer_position().unwrap();
+                    let offset_position = PhysicalPosition {
+                        x: current_position.x,
+                        y: current_position.y - 14,
+                    };
+                    let _ = window.set_position(tauri::Position::Physical(offset_position));
+                    window.show().unwrap();
+                    window.set_focus().unwrap();
+                },
+                SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                    "quit" => {
+                        std::process::exit(0);
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+        })
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::Focused(is_focused) => {
+                if !is_focused {
+                    event.window().hide().unwrap()
+                }
+            }
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,84 @@
+import { CaretDown, Plus } from '@phosphor-icons/react'
+import { useState } from 'react'
+
 export function App() {
+  const [isItemOpen, setIsItemOpen] = useState(false)
+
   return (
-    <div>
-      <h1 className="text-3xl text-purple-400">Rodrigo</h1>
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-[#1c1c1c] text-xs px-4 py-3">my todos</header>
+      <div>
+        <div className="py-1 border-b border-[#4a4a4a]">
+          <header className="px-4 py-3 flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              <button
+                className={`${isItemOpen ? 'rotate-180' : 'rotate-0'}`}
+                onClick={() => setIsItemOpen(!isItemOpen)}
+                type="button"
+              >
+                <CaretDown size={12} weight="bold" />
+              </button>
+              <strong className="text-xs">finish this app</strong>
+            </div>
+            <div className="flex items-center gap-2">
+              <span className="text-xs text-[#a1a1a1]">1/3</span>
+              <div className="bg-transparent w-3 h-3 border border-[#4a4a4a] rounded-full"></div>
+            </div>
+          </header>
+          {isItemOpen && (
+            <div className="pl-9 pr-4 pb-4 flex flex-col gap-2">
+              <label
+                htmlFor="task"
+                className="flex items-start gap-2 border border-[#4a4a4a] p-2 text-xs rounded"
+              >
+                <input type="checkbox" name="task" id="task" />
+                <span>code item component</span>
+              </label>
+              <label
+                htmlFor="task"
+                className="flex items-start gap-2 border border-[#4a4a4a] p-2 text-xs rounded"
+              >
+                <input type="checkbox" name="task" id="task" />
+                <span>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                  consectetur nulla non amet dolores, at esse modi, reiciendis,
+                  repudiandae vel veniam quasi in culpa quam adipisci corporis
+                  dolore voluptas. Ad.
+                </span>
+              </label>
+            </div>
+          )}
+        </div>
+        <div className="py-1 border-b border-[#4a4a4a]">
+          <header className="px-4 py-3 flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              <button type="button">
+                <CaretDown size={12} weight="bold" />
+              </button>
+              <strong className="text-xs">finish this app</strong>
+            </div>
+            <div className="flex items-center gap-2">
+              <span className="text-xs text-[#a1a1a1]">1/3</span>
+              <div className="bg-transparent w-3 h-3 border border-[#4a4a4a] rounded-full"></div>
+            </div>
+          </header>
+        </div>
+      </div>
+      <footer className="mt-auto p-4">
+        <form className="flex items-stretch gap-2 text-xs">
+          <input
+            className="p-2 flex-1 bg-[#313131] outline-none rounded"
+            placeholder="add new todo"
+            type="text"
+          />
+          <button
+            className="hover:bg-[#292929] flex items-center justify-center p-2 rounded"
+            type="submit"
+          >
+            <Plus size={18} />
+          </button>
+        </form>
+      </footer>
     </div>
-  );
+  )
 }
